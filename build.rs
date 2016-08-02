20decf85217d08a75a8250f1301cd4a435575b60
Connor Hilarides <connorcpu@live.com>
2016-08-02 00:13:40 -0700

    Fix compilation on MSVC

--- build.rs
+++ build.rs
@@ -1,9 +1,19 @@
 extern crate gcc;
 
+#[cfg(not(target_env = "msvc"))]
 fn main() {
     gcc::Config::new()
                 .file("src/accumulate.c")
                 .flag("-march=native")
                 .flag("-std=c99")
+                .compile("libaccumulate.a");
+}
+
+#[cfg(target_env = "msvc")]
+fn main() {
+    gcc::Config::new()
+                .file("src/accumulate.c")
+                // Maximum optimizations
+                .flag("/Ox")
                 .compile("libaccumulate.a");
 }
